ff5c59b0eabdab54a93d6ac486351e59bbdc7f11
michmi284 <58760931+michmi284@users.noreply.github.com>
2020-08-25 09:37:37 +0800

    111

--- js/index.js
+++ js/index.js
@@ -3,25 +3,19 @@ window.onload = function () {
         // 获取滚动条的纵坐标的值
         let s1 = document.documentElement.scrollTop;  // pc
         let s2 = document.body.scrollTop; // 移动
-        let scroll =  s1 == 0? s2: s1;
-        console.log("scroll"+ scroll)
+        let scroll = s1 == 0 ? s2 : s1;
+        console.log("scroll" + scroll)
         let search = document.querySelector('#search-fixxed-box')
         // 视口宽度
         let width = document.documentElement.clientWidth;
-        console.log("width*0.12"+ width*0.12)
-        if (scroll > width*0.12){
+        console.log("width*0.12" + width * 0.12)
+        if (scroll > width * 0.12) {
             // search变成固定定位
             search.style.position = 'fixed';
             search.style.left = 0;
             search.style.top = 0;
-        }else {
+        } else {
             search.style.position = 'static';
         }
-
-
-
-
     }
-
-
 }
